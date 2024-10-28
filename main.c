cfb5362628d0de9772e60cd70083b7bbe85b14bf
lucajk <lucajk95@gmail.com>
2024-10-28 17:47:54 +0000

    Adicionando variáveis globais

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+#define MAX_ALUNOS 100
+#define NOTA_MINIMA 0.0
+#define NOTA_MAXIMA 10.0
+#define MEDIA_APROVACAO 7.0
+#define FALTAS_MAXIMAS 20
+#define ARQUIVO "alunos.txt"
+#define ARQUIVO_BINARIO "alunos.bin"
